583d57def867d67d98403d158397b9aaea723ff0
TheStickMan1024 <leechinghung@gmail.com>
2025-12-14 18:22:42 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cvooy0mo89vt.us-east-1.rds.amazonaws.com "
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "leechingyi-employee"
 customregion = "us-east-1"
 
